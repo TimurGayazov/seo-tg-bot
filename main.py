6962afa63605c2841ce114d32c638a486fce07a3
Timur Gayazov <tim.gayazov@gmail.com>
2023-09-16 21:11:26 +0400

    base bot

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello muhamedji')
+import telebot
+
+bot = telebot.TeleBot('6630395700:AAEPNADLVlfc7HvH01pPDWJ81w5vAP4TNQw')
+print('The server is running')
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет', parse_mode='HTML')
+
+bot.polling(none_stop=True)
