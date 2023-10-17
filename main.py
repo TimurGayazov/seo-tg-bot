948ffe50f7f58617eebe0532eb3b00d06c5a7273
Timur Gayazov <tim.gayazov@gmail.com>
2023-10-17 23:52:03 +0400

    add func test

--- main.py
+++ main.py
@@ -5,12 +5,26 @@ bot = telebot.TeleBot('6630395700:AAEPNADLVlfc7HvH01pPDWJ81w5vAP4TNQw')
 print('The server is running')
 
 db_name = 'database.db'
-# keyboards
 flvl_keyboard_names = ["☃ Python", "☠ С++", "☹ Javascript", "❄ Профиль"]
+
+
 def first_level_keyboard():
     keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
-    # Сюда добавишь оставшиеся кнопки первого уровня
     keyboard.add(flvl_keyboard_names[0], flvl_keyboard_names[1], flvl_keyboard_names[2], flvl_keyboard_names[3])
+    return keyboard
+
+
+def second_level_keyboard():
+    keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+    keyboard.add('Начать')
+    keyboard.add('Назад')
+    return keyboard
+
+
+def third_level_keyboard():
+    keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+    keyboard.row('1', '2')
+    keyboard.row('3', '4')
     return keyboard
 
 
@@ -75,6 +89,7 @@ def create_database_test(message):
             f'INSERT INTO {table_name} (id_question, question, answers, right_answer) VALUES (?, ?, ?, ?);', table_data)
         connect.commit()
 
+
 def delete_database_test(message):
     message_list = message.text.split(' ')
     table_name = message_list[1]
@@ -82,6 +97,7 @@ def delete_database_test(message):
     cursor = connect.cursor()
     cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
     connect.commit()
+
 
 def read_test_info(message):
     message_list = message.text.split(' ')
@@ -93,12 +109,20 @@ def read_test_info(message):
     return rows
 
 
+def read_test_info_se():
+    table_name = 'python'
+    connect = sqlite3.connect(db_name)
+    cursor = connect.cursor()
+    cursor.execute(f'SELECT * FROM {table_name}')
+    rows = cursor.fetchall()
+    return rows
 
-# /start
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Привет', reply_markup=first_level_keyboard(), parse_mode='HTML')
     create_database(message)
+
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
@@ -123,14 +147,38 @@ def handle_text(message):
         for i in range(0, len(data)):
             bot.send_message(message.chat.id, f'Вопрос #{data[i][0]}\n\n{data[i][1]}\nВарианты ответов:{data[i][2]}\n\nПравильный ответ: {data[i][3]}')
 
+    elif '/testfunc' in message.text:
+        bot.send_message(message.chat.id, "Для того чтобы начать тестирование нажмите 'Начать' ", reply_markup=second_level_keyboard())
+        bot.register_next_step_handler(message, test_func)
+
     elif message.text == flvl_keyboard_names[0]:
         bot.send_message(message.chat.id, "Вы выбрали Python")
     elif message.text == flvl_keyboard_names[1]:
         bot.send_message(message.chat.id, "Вы выбрали C++")
     elif message.text == flvl_keyboard_names[2]:
         bot.send_message(message.chat.id, "Вы выбрали Javascript")
-    else :
+    else:
         bot.send_message(message.chat.id, "Не известная команда")
+
+
+def test_func(message, current_question=0):
+    data = read_test_info_se()
+    if current_question < len(data):
+        bot.send_message(message.chat.id, "Выберите правильный ответ: ", reply_markup=third_level_keyboard())
+        bot.send_message(message.chat.id, f'Вопрос #{data[current_question][0]}\n\n{data[current_question][1]}\nВарианты ответов:{data[current_question][2]}\n\nПравильный ответ: {data[current_question][3]}')
+        bot.register_next_step_handler(message, check_answer, current_question, data)
+    else:
+        bot.send_message(message.chat.id, "Тестирование окончено!", reply_markup=first_level_keyboard())
+
+
+def check_answer(message, current_question, data):
+    if message.text == data[current_question][3]:
+        bot.send_message(message.chat.id, "Ответ принят.")
+    else:
+        bot.send_message(message.chat.id, "Ответ не принят.")
+
+    current_question += 1
+    test_func(message, current_question)
 
 
 bot.polling(none_stop=True)
